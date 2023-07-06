86b63f222fd81dc5988ab9407dfdee726187d772
Abhijit <abhi.baldawa@gmail.com>
2023-07-06 17:51:12 +0100

    Added JSDOC for the service methods performing invoice calculations

--- server/src/app/modules/project-invoice/service.ts
+++ server/src/app/modules/project-invoice/service.ts
@@ -9,6 +9,15 @@ import * as projectPhaseModel from '../../database/models/project-phases';
 import * as costItemModel from '../../database/models/cost-items';
 import * as projectModel from '../../database/models/projects';
 
+/**
+ * @public
+ *
+ * Calculates the total cost of the cost item
+ * based on its `billedBy` property which is
+ * either by `HOUR` or `UNITS`
+ *
+ * @param billedBy - billing cost information for a cost item
+ */
 const getTotalCostOfCostItem = (
   billedBy: costItemModel.CostItem['billedBy']
 ) => {
@@ -18,6 +27,16 @@ const getTotalCostOfCostItem = (
   return billedBy.totalUnits * billedBy.costPerUnit;
 };
 
+/**
+ * @public
+ *
+ * Calculates the subtotal price for a single project phase
+ * by including any available discount or extra incurred fee
+ * for the phase
+ *
+ * @param projectPhase - phased for which subtotal price
+ *                       needs to be calculated
+ */
 const getProjectPhaseSubtotalPrice = (
   projectPhase: Pick<projectPhaseModel.ProjectPhase, 'id' | 'discountOrFee'>
 ) => {
@@ -40,6 +59,16 @@ const getProjectPhaseSubtotalPrice = (
   return totalItemsCost + discountOrFee;
 };
 
+/**
+ * @public
+ *
+ * Calculates the subtotal tax for a single project phase
+ * by applying applicable tax percentages to individual
+ * cost items for that phases
+ *
+ * @param projectPhaseId - phased id for which subtotal tax
+ *                         needs to be calculated
+ */
 const getProjectPhaseSubtotalTax = (
   projectPhaseId: projectPhaseModel.ProjectPhase['id']
 ) =>
@@ -53,6 +82,17 @@ const getProjectPhaseSubtotalTax = (
       );
     }, 0);
 
+/**
+ * @public
+ *
+ * Calculates the entire project subtotal price by
+ * calculating the subtotal price of individual phases
+ * including their discounts/fees and also takes into
+ * account the project level discount/fee
+ *
+ * @param project - project for which subtotal price needs
+ *                  to be calculated
+ */
 const getProjectSubtotalPrice = (
   project: Pick<projectModel.Project, 'id' | 'discountOrFee'>
 ) => {
@@ -78,6 +118,15 @@ const getProjectSubtotalPrice = (
   return totalPhasesCost + discountOrFee;
 };
 
+/**
+ * @public
+ *
+ * Calculates the entire project tax by calculating the
+ * subtotal tax of each individual phase for that project
+ *
+ * @param projectId - id of the project for which total tax needs
+ *                    to be calculated
+ */
 const getProjectTax = (projectId: projectModel.Project['id']) => {
   const projectPhases =
     projectPhaseModel.getProjectPhasesByProjectId(projectId);
@@ -91,6 +140,19 @@ const getProjectTax = (projectId: projectModel.Project['id']) => {
   return totalPhasesTax;
 };
 
+/**
+ * @public
+ *
+ * Calculates the final price of the entire project by
+ * calculating the subtotal price of the project based on
+ * the subtotal price of individual phases and total incurred
+ * tax for individual cost items within every project phase.
+ *
+ * This also takes into account all the discount/fee for all the
+ * phases and one time discount/fee at the project level
+ *
+ * @param project - project for which the total prices needs to be calculated
+ */
 const getProjectTotalPrice = (
   project: Pick<projectModel.Project, 'id' | 'discountOrFee'>
 ) => getProjectSubtotalPrice(project) + getProjectTax(project.id);
